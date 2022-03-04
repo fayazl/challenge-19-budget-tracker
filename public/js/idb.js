7542ca20915b3690d2456dc46de056006bbde7dd
Fayaz <flalani100@gmail.com>
2022-03-03 19:30:21 -0500

    Started idb.js file

---
+++ public/js/idb.js
@@ -0,0 +1,35 @@
+let db;
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run checkDatabase() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
+
+request.onerror = function(event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+
+  const budgetObectStore = transaction.objectStore('new_budget');
+
+  // add record to your store with add method.
+  budgetObectStore.add(record);
+}
+
+
+// listen for app coming back online
+window.addEventListener('online', uploadBudget);
--- public/service-worker.js
+++ public/service-worker.js
@@ -13,7 +13,8 @@ const FILES_TO_CACHE = [
   "./icons/icon-192x192.png",
   "./icons/icon-384x384.png",
   "./icons/icon-512x512.png",
-  "./js/index.js"
+  "./js/index.js",
+  "./js/idb.js"
 ];
 
 // Respond with cached resources
