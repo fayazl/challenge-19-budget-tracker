956c3f01b670abed0942ca60cbb002cecac61732
Fayaz <flalani100@gmail.com>
2022-03-03 19:30:53 -0500

    Added uploadBudget function

--- public/js/idb.js
+++ public/js/idb.js
@@ -31,5 +31,45 @@ function saveRecord(record) {
 }
 
 
+function uploadBudget() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+  
+    // access your pending object store
+    const budgetObectStore = transaction.objectStore('new_budget');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObectStore.getAll();
+  
+    getAll.onsuccess = function() {
+      // if there was data in indexedDb's store, let's send it to the api server
+      if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(serverResponse => {
+            if (serverResponse.message) {
+              throw new Error(serverResponse);
+            }
+  
+            const transaction = db.transaction(['new_budget'], 'readwrite');
+            const budgetObectStore = transaction.objectStore('new_budget');
+            // clear all items in your store
+            budgetObectStore.clear();
+          })
+          .catch(err => {
+            // set reference to redirect back here
+            console.log(err);
+          });
+      }
+    };
+  }
+
 // listen for app coming back online
 window.addEventListener('online', uploadBudget);
